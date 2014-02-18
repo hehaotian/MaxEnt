515a2865cfdde3eca956770a0028d0a5c35ca918
Haotian He <haotianh@uw.edu>
2014-02-17 21:54:45 -0800

    fifth commit

--- MaxEnt.java
+++ MaxEnt.java
@@ -15,7 +15,7 @@ import java.lang.*;
 public class MaxEnt {
 
 	private Map<String, Map<String, Double>> model = new HashMap<String, Map<String, Double>>();
-	private Map<String, Map<String, Integer>> test_data = new HashMap<String, Map<String, Integer>>();
+	// private Map<String, Map<String, Integer>> test_data = new HashMap<String, Map<String, Integer>>();
   private Map<String, Map<String, Integer>> test_matrix = new HashMap<String, Map<String, Integer>>();
 	private Set<String> classLabs = new TreeSet<String>();
   private Set<String> feature_counts = new TreeSet<String>();
@@ -49,14 +49,86 @@ public class MaxEnt {
       System.out.print(class_4 + " ");
     }
     */
-    
+
 		return model;
 	}
 
-	public void predict(String test_path, PrintStream res) throws IOException {
-		BufferedReader test_file = new BufferedReader(new FileReader(test_path));
-		res.println("res file test");
-	}
+	public void predict(String file_path, PrintStream ps) throws IOException {
+      
+    BufferedReader br = new BufferedReader(new FileReader(file_path));
+    ps.println("%%%%% test data:");
+      
+    String line = "";
+    String correct_classLabel = "";
+    int instanceName = -1;
+    while ((line = br.readLine()) != null) {
+         
+      instanceName ++;
+      line = line.replaceAll("[\\s]+", " ");
+      String[] tokens = line.split(" ");
+      correct_classLabel = tokens[0];
+      
+      Map<String, Double> temp_pred_probs = new HashMap<String, Double>();
+      
+      double normal_z = 0.0;   
+      Iterator itr = classLabs.iterator();
+      while (itr.hasNext()) {
+        String label = "" + itr.next();
+        double sum = 0.0;
+            
+        for (int i = 1; i < tokens.length; i++) {
+          String word = tokens[i].replaceAll(":[\\d]+", "");
+          sum += model.get(label).get(word);
+        }
+        double result = exp(sum);
+        pred_probs.put(label, result);
+        normal_z += result;
+      }
+
+      Map<String, Double> final_pred_probs = new HashMap<String, Double>();
+
+      Iterator itr = classLabs.iterator();
+      while (itr.hasNext()) {
+        String label = "" + itr.next();
+        final_pred_probs.put(label, temp_pred_probs.get(label) / normal_z);
+      }
+  
+      Map<String, String> descend_probs = sortByComparator(final_pred_probs);
+      ps.println("array:" + instanceName + " " + correct_classLabel); 
+      int counter = 0;
+      for (Map.Entry entry : descend_probs.entrySet()) {
+        ps.print(" " + entry.getKey() + " " + entry.getValue());
+        ps.println();
+        String key = "" + entry.getKey(); // predicted class
+        counter ++;
+        if (counter == 1) {
+          if (!test_matrix.containsKey(correct_classLabel)) {
+            test_matrix.put(correct_classLabel, new HashMap<String, Integer>());
+          } else if (test_matrix.get(correct_classLabel).containsKey(key)) {
+            test_matrix.get(correct_classLabel).put(key, test_matrix.get(correct_classLabel).get(key) + 1);
+          } else {
+            test_matrix.get(correct_classLabel).put(key, 1);
+          }
+        }
+      }
+    }
+  }
+   
+  private Map sortByComparator(Map unsortMap) {
+    List list = new LinkedList(unsortMap.entrySet());
+    Collections.sort(list, new Comparator() {
+      public int compare(Object o1, Object o2) {
+        return ((Comparable) ((Map.Entry) (o2)).getValue())
+        .compareTo(((Map.Entry) (o1)).getValue());
+      }
+    });
+    Map sortedMap = new LinkedHashMap();
+    for (Iterator it = list.iterator(); it.hasNext();) {
+      Map.Entry entry = (Map.Entry) it.next();
+      sortedMap.put(entry.getKey(), entry.getValue());
+    }
+    return sortedMap;
+   }
 
 	public void report() {
       System.out.println("class_num=" + classLabs.size() + " feat_num=" + feature_counts.size());
